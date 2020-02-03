3ae181db037f8691f7831aba112a7b2fa32f7532
tovinodsharma <dev.vinod@outlook.com>
2020-02-03 23:26:27 +0530

    statistics

--- src/main/java/org/dockerwell/statistics/Statistics.java
+++ src/main/java/org/dockerwell/statistics/Statistics.java
@@ -1,5 +1,14 @@
 package org.dockerwell.statistics;
 
-public class Statistics {
+import java.util.List;
+import java.util.OptionalDouble;
 
+public class Statistics {
+	public Double populationMean(List<Double> groupCharaceterstics) {
+		OptionalDouble mean = null;
+		if (!groupCharaceterstics.isEmpty()) {
+			mean = groupCharaceterstics.stream().mapToDouble(c -> c).average();
+		}
+		return mean.getAsDouble();
+	}
 }
--- src/test/java/org/dockerwell/test/statistics/StatisticsTest.java
+++ src/test/java/org/dockerwell/test/statistics/StatisticsTest.java
@@ -1,5 +1,33 @@
 package org.dockerwell.test.statistics;
 
-public class StatisticsTest {
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dockerwell.statistics.Statistics;
+import org.junit.Test;
+
+public class StatisticsTest {
+	
+	@Test
+	public void populationMeanTest() {
+		Statistics statistics = new Statistics();
+		List<Double> a= new ArrayList<Double>();
+		a.add(1.0);
+		a.add(2.0);
+		a.add(3.0);
+		a.add(4.0);
+		a.add(5.0);
+		a.add(6.0);
+		a.add(7.0);
+		a.add(8.0);
+		a.add(9.0);
+		a.add(10.0);
+		a.add(11.0);
+		a.add(12.0);
+		
+		assertTrue(statistics.populationMean(a) == 6.5 ? true : false);
+	}
+	
 }
